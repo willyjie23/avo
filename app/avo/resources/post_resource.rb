f3f5e8c7cae497d08c47255aa46e1c2cc0803209
willy chang <willy.chang@solmate.cc>
2021-05-30 14:21:23 +0800

    add avo gem

---
+++ app/avo/resources/post_resource.rb
@@ -0,0 +1,10 @@
+class PostResource < Avo::BaseResource
+  self.title = :name
+  self.includes = []
+  # self.search_query = ->(params:) do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # add fields here
+end
